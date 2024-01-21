a0d98ccf9601f939e6af013ea03ec6d6d57db519
Antonello Zanini <antonello@writech.run>
2024-01-21 18:49:33 +0100

    Bug fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-const express = require('express');
+const express = require("express");
 
 // initialize an Express app
 const app = express();
+app.use(express.json());
 
 // sample data to simulate a database
 let users = [
